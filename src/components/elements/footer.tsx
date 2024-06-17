a0f3b3555ffc238fb9d1c5d74a950f12df23377f
Annava Wisha Sikoko <annava.wisha@ui.ac.id>
2024-06-17 20:13:05 +0700

    style: animation for footer

--- src/components/elements/footer.tsx
+++ src/components/elements/footer.tsx
@@ -4,7 +4,7 @@ import { poppins } from "@/styles/fonts";
 
 const Footer = () => {
   return (
-    <div className="bg-black backdrop-blur-lg">
+    <div className={`bg-black backdrop-blur-lg ${poppins.className} `}>
       <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
         <div className="md:flex md:justify-between">
           <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3"></div>
@@ -18,7 +18,7 @@ const Footer = () => {
             <a
               href="https://github.com/annavaws/my-portfolio"
               target="_blank"
-              className="text-gray-100 hover:text-purple-500 ms-5"
+              className="text-gray-100 hover:text-purple-500 hover:scale-105 ease-in-out duration-300 ms-5"
             >
               <svg
                 className="w-6 h-6"
@@ -33,13 +33,13 @@ const Footer = () => {
                   clipRule="evenodd"
                 />
               </svg>
-              <span className="sr-only ">GitHub account</span>
+              <span className="sr-only">GitHub account</span>
             </a>
 
             <a
               href="https://www.linkedin.com/in/annava-ws/"
               target="_blank"
-              className="text-gray-100 hover:text-purple-500 ms-5"
+              className="text-gray-100 hover:text-purple-500 hover:scale-105 ease-in-out duration-300 ms-4"
             >
               <svg
                 className="w-7 h-7"
@@ -50,30 +50,28 @@ const Footer = () => {
               >
                 <path d="M41,4H9C6.24,4,4,6.24,4,9v32c0,2.76,2.24,5,5,5h32c2.76,0,5-2.24,5-5V9C46,6.24,43.76,4,41,4z M17,20v19h-6V20H17z M11,14.47c0-1.4,1.2-2.47,3-2.47s2.93,1.07,3,2.47c0,1.4-1.12,2.53-3,2.53C12.2,17,11,15.87,11,14.47z M39,39h-6c0,0,0-9.26,0-10 c0-2-1-4-3.5-4.04h-0.08C27,24.96,26,27.02,26,29c0,0.91,0,10,0,10h-6V20h6v2.56c0,0,1.93-2.56,5.81-2.56 c3.97,0,7.19,2.73,7.19,8.26V39z"></path>
               </svg>
-              <span className="sr-only ">LinkedIn account</span>
+              <span className="sr-only">LinkedIn account</span>
             </a>
 
             <a
               href="https://mail.google.com/mail/?view=cm&fs=1&to=annavaws@gmail.com"
               target="_blank"
-              className="text-gray-100 hover:text-purple-500 ms-5"
+              className="text-gray-100 hover:text-purple-500 hover:scale-105 ease-in-out duration-300 mt-1 ms-3"
             >
               <svg
                 className="w-10 h-10"
-                viewBox="0 0 24 24"
+                viewBox="0 0 26 26"
                 fill="currentColor"
                 xmlns="http://www.w3.org/2000/svg"
                 aria-hidden="true"
               >
                 <path
                   d="M3 8L8.44992 11.6333C9.73295 12.4886 10.3745 12.9163 11.0678 13.0825C11.6806 13.2293 12.3194 13.2293 12.9322 13.0825C13.6255 12.9163 14.2671 12.4886 15.5501 11.6333L21 8M6.2 19H17.8C18.9201 19 19.4802 19 19.908 18.782C20.2843 18.5903 20.5903 18.2843 20.782 17.908C21 17.4802 21 16.9201 21 15.8V8.2C21 7.0799 21 6.51984 20.782 6.09202C20.5903 5.71569 20.2843 5.40973 19.908 5.21799C19.4802 5 18.9201 5 17.8 5H6.2C5.0799 5 4.51984 5 4.09202 5.21799C3.71569 5.40973 3.40973 5.71569 3.21799 6.09202C3 6.51984 3 7.07989 3 8.2V15.8C3 16.9201 3 17.4802 3.21799 17.908C3.40973 18.2843 3.71569 18.5903 4.09202 18.782C4.51984 19 5.07989 19 6.2 19Z"
-                  stroke="#000000"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
+                  stroke="black"
+                  strokeWidth="1.5"
                 />
               </svg>
-              <span className="sr-only ">LinkedIn account</span>
+              <span className="sr-only">LinkedIn account</span>
             </a>
           </div>
         </div>
