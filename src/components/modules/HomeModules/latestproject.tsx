9766a10cfd3f4d2528040ef620feae5ab8b80d3f
Annava Wisha Sikoko <annava.wisha@ui.ac.id>
2024-07-22 00:50:42 +0700

    feat: change background transparancy

--- src/components/modules/HomeModules/latestproject.tsx
+++ src/components/modules/HomeModules/latestproject.tsx
@@ -58,72 +58,70 @@ export default function Latest() {
   ];
 
   return (
-    <div className="p-10 md:p-12 lg:p-20 bg-gradient-to-t from-black to-gray-900">
+    <div className="p-10 lg:p-20 bg-gradient-to-t from-black to-gray-900">
       <div
-        className={`${albert.className} flex justify-center flex-col backdrop-blur-xl text-slate-200`}
+        className={`${albert.className} flex justify-center w-full flex-col backdrop-blur-xl text-slate-200`}
       >
-        <div className="flex w-full font-semibold text-5xl md:text-5xl">
+        <div className="px-16 flex w-full font-semibold text-5xl md:text-5xl mb-10">
           Latest Projects
         </div>
-        <div className="flex text-slate-200">
-          <div className="mt-20">
-            <div className="grid grid-cols-1 lg:grid-cols-2 gap-24 lg:gap-12">
-              {projects.map((project) => (
-                <CardContainer
-                  key={project.id}
-                  className="inter-var px-10 -mt-20"
-                >
-                  <CardBody className="relative group/card hover:shadow-2xl hover:shadow-emerald-500/[0.1] bg-black w-auto sm:w-[30rem] h-auto rounded-xl p-6 border">
-                    <CardItem className="text-xl font-bold">
-                      {project.title}
+        <div className="px-0 lg:px-12 flex text-slate-200">
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-24 lg:gap-12">
+            {projects.map((project) => (
+              <CardContainer
+                key={project.id}
+                className="inter-var px-10 -mt-20"
+              >
+                <CardBody className="relative group/card hover:shadow-2xl hover:shadow-purple-500/20 bg-black/30 w-auto sm:w-[30rem] h-auto rounded-xl p-6 border">
+                  <CardItem className="text-xl font-bold">
+                    {project.title}
+                  </CardItem>
+                  <CardItem
+                    as="p"
+                    className="text-sm max-w-sm mt-2 text-neutral-300"
+                  >
+                    {project.description}
+                  </CardItem>
+                  <CardItem className="w-full mt-4">
+                    <Image
+                      src={project.image}
+                      height="1000"
+                      width="1000"
+                      className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
+                      alt={project.title}
+                    />
+                  </CardItem>
+                  <div className="flex flex-wrap gap-2 mt-4">
+                    {project.techStack.map((tech, index) => (
+                      <CardItem
+                        key={index}
+                        className="bg-transparent border border-white text-white rounded-full px-4 py-2 text-xs"
+                      >
+                        {tech}
+                      </CardItem>
+                    ))}
+                  </div>
+                  <div className="flex justify-between items-center mt-10">
+                    <CardItem
+                      as="a"
+                      target="_blank"
+                      href={project.live}
+                      className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-normal font-bold hover:bg-slate-500"
+                    >
+                      Live
                     </CardItem>
                     <CardItem
-                      as="p"
-                      className="text-sm max-w-sm mt-2 text-neutral-300"
+                      as="a"
+                      target="_blank"
+                      href={project.repo}
+                      className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-normal font-bold hover:bg-slate-500"
                     >
-                      {project.description}
+                      Repo
                     </CardItem>
-                    <CardItem className="w-full mt-4">
-                      <Image
-                        src={project.image}
-                        height="1000"
-                        width="1000"
-                        className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
-                        alt={project.title}
-                      />
-                    </CardItem>
-                    <div className="flex flex-wrap gap-2 mt-4">
-                      {project.techStack.map((tech, index) => (
-                        <CardItem
-                          key={index}
-                          className="bg-transparent border border-white text-white rounded-full px-4 py-2 text-xs"
-                        >
-                          {tech}
-                        </CardItem>
-                      ))}
-                    </div>
-                    <div className="flex justify-between items-center mt-10">
-                      <CardItem
-                        as="a"
-                        target="_blank"
-                        href={project.live}
-                        className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-normal font-bold hover:bg-slate-500"
-                      >
-                        Live
-                      </CardItem>
-                      <CardItem
-                        as="a"
-                        target="_blank"
-                        href={project.repo}
-                        className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-normal font-bold hover:bg-slate-500"
-                      >
-                        Repo
-                      </CardItem>
-                    </div>
-                  </CardBody>
-                </CardContainer>
-              ))}
-            </div>
+                  </div>
+                </CardBody>
+              </CardContainer>
+            ))}
           </div>
         </div>
       </div>
